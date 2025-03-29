bb16aeccb3b5f34f829937d83908a0ff1d57b0e4
xnileshtiwari <135645478+xnileshtiwari@users.noreply.github.com>
2025-03-29 23:55:12 +0530

    Update generative.py

--- libs/generative.py
+++ libs/generative.py
@@ -1,9 +1,16 @@
 from langchain_google_genai import ChatGoogleGenerativeAI, HarmBlockThreshold, HarmCategory
 from dotenv import load_dotenv
+from langchain_core.rate_limiters import InMemoryRateLimiter
 
+rate_limiter = InMemoryRateLimiter(
+    requests_per_second=0.1,  # <-- Super slow! We can only make a request once every 10 seconds!!
+    check_every_n_seconds=0.1,  # Wake up every 100 ms to check whether allowed to make a request,
+    max_bucket_size=10,  # Controls the maximum burst size.
+)
+import os 
 load_dotenv()
 
-
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-pro-exp-03-25",
     safety_settings={
@@ -11,4 +18,5 @@ llm = ChatGoogleGenerativeAI(
     },
     verbose=True,
     api_key=GOOGLE_API_KEY,
+    rate_limiter=rate_limiter
 )
